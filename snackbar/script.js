edeadb1310306b0de7263626cb7aa1baababa7c1
Alexssandro Bianchi <alexbbianchi@gmail.com>
2023-06-28 16:36:00 -0300

    feat: added copy code

--- snackbar/script.js
+++ snackbar/script.js
@@ -1,9 +1,13 @@
-function snackbarTest() {
-    snackbar({ message: "Test" });
+const copyCode = () => {
+    const textInsert = document.querySelector('#textInsert');
+
+    navigator.clipboard.writeText(textInsert.value)
+        .then(() => snackbar({ message: 'Content copied to clipboard' }))
+        .catch((error) => snackbar({ message: 'Failed to copy content: ' + error, backgroundColor: '#f44336' }));
 }
 
-function snackbar ({ message, backgroundColor = '#5edc9d', timeout = 3000 }) {
-    var snackbar = document.createElement('div');
+const snackbar = ({ message, backgroundColor = '#5edc9d', timeout = 3000 }) => {
+    const snackbar = document.createElement('div');
     
     snackbar.innerHTML = message;
     snackbar.classList.add('snackbar');
